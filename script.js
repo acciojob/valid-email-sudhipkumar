d74b55c1a72225485145bcedfe8ee1c8c6d100b6
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-26 21:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function validEmail(str) {
   //your JS code here.
 	// Regular expression to validate email format
+	const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+	 return emailRegex.test(str);
 }
 
 // Do not change the code below.
